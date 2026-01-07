9091ee39512f30c383b44c811150da9d01f474be
Akin Saye Sokpah <assokpah@gmail.com>
2026-01-07 02:04:38 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,7 @@ export default function MyApp({ Component, pageProps }) {
           name="google-site-verification"
           content="Xph8kvaL-aAkTHe30pd74SqDHgdUFGDx7p3TLie_LTI"
         />
-        <title>BioNurse – Free Nursing Study App for Students Worldwide</title>
-        <meta
-          name="description"
-          content="BioNurse is a free nursing study app for nursing students with notes, quizzes, courses, scholarships, and exam preparation worldwide."
-        />
       </Head>
-
       <Component {...pageProps} />
     </>
   );
